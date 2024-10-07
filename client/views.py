ba174925fdd279779b5af97a7bc6832feef43139
Mahya Abouei <mahyaaboueii@gmail.com>
2024-10-07 13:06:04 +0330

    view dataframe

--- client/views.py
+++ client/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import status 
 from rest_framework.views import APIView
-from . import serializers
+from .serializers import  ClientSerializer
 import datetime
 from . import models
 import requests
 import pandas as pd
 import random
+
 
 
 frm ='30001526'
@@ -16,7 +17,7 @@ psswrd ='5246043adeleh'
 
 # sms for uuid
 def SendSms(snd,txt):
-    txt = f'به کارگزاری ایساتیس پویا خوش آمدید \n ورود:\n www.isatispooya.com/{txt}/'
+    txt = f'به کارگزاری ایساتیس پویا خوش آمدید \n ورود:\n www.gift.isatispooya.com/{txt}/'
     resp = requests.get(url=f'http://tsms.ir/url/tsmshttp.php?from={frm}&to={snd}&username={usrnm}&password={psswrd}&message={txt}').json()
     print(txt)
     return resp
@@ -32,7 +33,7 @@ class ClientViewset(APIView):
             return Response({'error':'این شماره موبایل قبلا ثبت شده است'},status=status.HTTP_400_BAD_REQUEST)
         else : 
             client = models.Client.objects.create(mobile=mobile)
-            serializer = serializers.ClientSerializer(client)
+            serializer =ClientSerializer(client)
             SendSms(mobile,client.uuid)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
@@ -57,15 +58,18 @@ class CheckUuidViewset(APIView) :
 
 # sms for gift
 def SendSms2(mobile, gift):
-    txt = 'به کارگزاری ایساتیس پویا خوش آمدید \n'
-    txt += 'ادرس سایت:\n www.isatispooya.com/ \n'
+    txt = 'بازدید کننده گرامی \n'
     txt += 'هدیه شما:\n'
     txt += f'{gift}\n'  
+    txt += 'برای تخصیص حداکثر تا پایان مهرماه ثبت نام خود را در کارگزاری ایساتیس پویا تکمیل کنید\n'
     resp = requests.get(
         url=f'http://tsms.ir/url/tsmshttp.php?from={frm}&to={mobile}&username={usrnm}&password={psswrd}&message={txt}'
     ).json()
     print(txt)
     return resp
+
+
+
 
 # lottery and give gift
 class GiftViewset(APIView):
@@ -84,8 +88,10 @@ class GiftViewset(APIView):
         answer = request.data.get('answer')
         # if answer >2 or answer <= 0 or not answer  :
         #     return Response({'error':'لطفا پاسخ را به صورت عددی وارد کنید'},status=status.HTTP_400_BAD_REQUEST)
+
         
-        df = pd.read_excel('book1.xlsx')
+        exel = [{'جواب': 0, 'ویسا': 10, 'بازرگام': 5, 'مفتول': 0, 'خاتم': 0, 'ترمه': 0, 'min': 0, 'max': 35}, {'جواب': 0, 'ویسا': 10, 'بازرگام': 5, 'مفتول': 25, 'خاتم': 0, 'ترمه': 0, 'min': 35, 'max': 42}, {'جواب': 0, 'ویسا': 10, 'بازرگام': 5, 'مفتول': 25, 'خاتم': 10, 'ترمه': 10, 'min': 42, 'max': 44}, {'جواب': 0, 'ویسا': 10, 'بازرگام': 15, 'مفتول': 25, 'خاتم': 0, 'ترمه': 0, 'min': 44, 'max': 47}, {'جواب': 0, 'ویسا': 25, 'بازرگام': 25, 'مفتول': 0, 'خاتم': 0, 'ترمه': 0, 'min': 47, 'max': 87}, {'جواب': 0, 'ویسا': 25, 'بازرگام': 25, 'مفتول': 50, 'خاتم': 0, 'ترمه': 0, 'min': 87, 'max': 95}, {'جواب': 0, 'ویسا': 25, 'بازرگام': 25, 'مفتول': 50, 'خاتم': 10, 'ترمه': 10, 'min': 95, 'max': 97}, {'جواب': 0, 'ویسا': 25, 'بازرگام': 30, 'مفتول': 50, 'خاتم': 0, 'ترمه': 0, 'min': 97, 'max': 100}, {'جواب': 1, 'ویسا': 50, 'بازرگام': 30, 'مفتول': 0, 'خاتم': 0, 'ترمه': 0, 'min': 0, 'max': 35}, {'جواب': 1, 'ویسا': 50, 'بازرگام': 30, 'مفتول': 100, 'خاتم': 0, 'ترمه': 0, 'min': 35, 'max': 42}, {'جواب': 1, 'ویسا': 50, 'بازرگام': 30, 'مفتول': 100, 'خاتم': 20, 'ترمه': 10, 'min': 42, 'max': 44}, {'جواب': 1, 'ویسا': 50, 'بازرگام': 45, 'مفتول': 100, 'خاتم': 0, 'ترمه': 0, 'min': 44, 'max': 47}, {'جواب': 1, 'ویسا': 75, 'بازرگام': 40, 'مفتول': 0, 'خاتم': 0, 'ترمه': 0, 'min': 47, 'max': 87}, {'جواب': 1, 'ویسا': 75, 'بازرگام': 40, 'مفتول': 150, 'خاتم': 0, 'ترمه': 0, 'min': 87, 'max': 95}, {'جواب': 1, 'ویسا': 75, 'بازرگام': 40, 'مفتول': 150, 'خاتم': 30, 'ترمه': 30, 'min': 95, 'max': 97}, {'جواب': 1, 'ویسا': 75, 'بازرگام': 70, 'مفتول': 150, 'خاتم': 0, 'ترمه': 0, 'min': 97, 'max': 100}, {'جواب': 2, 'ویسا': 100, 'بازرگام': 60, 'مفتول': 0, 'خاتم': 0, 'ترمه': 0, 'min': 0, 'max': 75}, {'جواب': 2, 'ویسا': 100, 'بازرگام': 60, 'مفتول': 200, 'خاتم': 0, 'ترمه': 0, 'min': 75, 'max': 90}, {'جواب': 2, 'ویسا': 100, 'بازرگام': 100, 'مفتول': 200, 'خاتم': 40, 'ترمه': 40, 'min': 90, 'max': 95}, {'جواب': 2, 'ویسا': 100, 'بازرگام': 140, 'مفتول': 200, 'خاتم': 0, 'ترمه': 0, 'min': 95, 'max': 100}]
+        df = pd.DataFrame(exel)
         df = df[df['جواب']==answer]
         df['max'] = df['max'].apply(int)
         df['min'] = df['min'].apply(int)
--- exhibition/settings.py
+++ exhibition/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -144,3 +144,8 @@ CORS_ALLOW_METHODS = [
 CORS_ALLOW_HEADERS = [
     '*'
 ]
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
